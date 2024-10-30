a3a0735a9349cdac271510ad0d76d2fb644f232a
susik17 <susikumar1718@gmail.com>
2024-10-30 15:43:52 +0530

    added files

---
+++ Arrays/Trappingrainwater.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class Trappingrainwater {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        printarray(n,arr);
+        
+
+
+    }
+    public void printarray(int num,int[] arr){
+        for(int i=0;i<num;i++){
+            System.out.print(arr[i]+" ");
+        }
+        
+
+    }
+    
+    
+}
---
+++ patterns/rectangularstar.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class rectangularstar {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(n);
+        for(int i=1;i<=n;i++)
+        {
+            for (int j=1;j<=n;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+    
+}
+
+/*
+
+
+n=4
+****
+****
+****
+****
+
+ */

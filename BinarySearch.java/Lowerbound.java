b7c96d49427990c0c6b5b206392689806e8ed8f1
susik17 <susikumar1718@gmail.com>
2024-12-23 18:35:42 +0530

    files are added on 23 December

---
+++ BinarySearch.java/Lowerbound.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class LowerBound{
+     static int lowerBound(int[] arr, int n, int x) {
+        int low=0;
+        int high=n-1;
+        int ans=n;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]>=x){
+                ans=mid;
+                high=high-1;
+            }
+            else{
+                low=low+1;
+            }
+        }
+        return ans;
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int x=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<=n-1;i++){
+            arr[i]=sc.nextInt();
+        }
+        
+        System.out.print(lowerBound(arr,n,x));
+        
+        
+    }
+}
+/*
+ip:
+5 5
+1 4 6 10 13
+op:
+2
+*/
